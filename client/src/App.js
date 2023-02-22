093156032e5a790a18344d153a02486a88803657
xtombrock <xtombrock1989@gmail.com>
2023-02-22 17:28:19 +0000

    modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
+// Pages
 import Home from './Home';
 import ContactOne from './pages/contact/ContactOne';
+import HeaderOne from './pages/headers/HeaderOne';
+import LoginOne from './pages/login/LoginOne';
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} index />
         <Route path='/contact-one' element={<ContactOne />} />
+        <Route path='/login-one' element={<LoginOne />} />
+        <Route path='/header-one' element={<HeaderOne />} />
       </Routes>
     </>
   );
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -5,7 +5,17 @@ function Home() {
   return (
     <div className='grid'>
       <h1>Pages Container</h1>
-      <Link to='/contact-one'>Contact One</Link>
+      <section>
+        <div>
+          <Link to='/contact-one'>Contact One</Link>
+        </div>
+        <div>
+          <Link to='/login-one'>Login One</Link>
+        </div>
+        <div>
+          <Link to='/header-one'>Header One</Link>
+        </div>
+      </section>
     </div>
   );
 }
